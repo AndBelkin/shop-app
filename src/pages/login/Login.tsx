0378562e71a8c6b2aba021dcf113672c00745734
AndBelkin <dev.andreubelkin@gmail.com>
2025-11-08 14:57:25 +0300

    add state & handle for input (Login)

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,19 +1,43 @@
-import type { FC, FormEvent } from "react";
+import { useState, type ChangeEvent, type FC, type FormEvent } from "react";
 import "./Login.css";
 import Button from "../../components/button/Button";
 import InputText from "../../components/inputText/InputText";
 
 const Login: FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => setEmail(event.target.value);
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => setPassword(event.target.value);
   const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => event.preventDefault();
+  const handleButtonClick = () => console.log({ email, password });
   return (
     <main className="login">
       <section className="login-section">
         <form className="login-form" onSubmit={handleFormSubmit}>
           <h2 className="login-form__title">Log In</h2>
           <p className="login-form__subtitle">For buying drinks and beverages. Login first, please.</p>
-          <InputText type="text" label="Email" placeholder="Your email..." />
-          <InputText type="password" label="Password" placeholder="Your password..." />
-          <Button view="accent" onClick={() => {}}>
+          <InputText
+            type="text"
+            name="email"
+            label="Email"
+            placeholder="Your email..."
+            value={email}
+            onChange={handleEmailChange}
+            onClear={() => setEmail("")}
+          />
+          <InputText
+            type="password"
+            name="password"
+            label="Password"
+            placeholder="Your password..."
+            value={password}
+            onChange={handlePasswordChange}
+            onClear={() => setPassword("")}
+            minLength={6}
+            maxLength={30}
+          />
+          <Button view="accent" onClick={handleButtonClick}>
             Log In
           </Button>
           <Button view="none" onClick={() => {}}>
