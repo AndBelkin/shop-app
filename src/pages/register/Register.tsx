848d2b3f30c712a1ceff463d6d4db0ecaf270f30
AndBelkin <dev.andreubelkin@gmail.com>
2025-11-08 15:08:28 +0300

    add state & handle for input (Register)

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -6,7 +6,14 @@ import InputText from "../../components/inputText/InputText";
 const Register: FC = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
   const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => event.preventDefault();
+  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => setUsername(event.target.value);
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => setEmail(event.target.value);
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => setPassword(event.target.value);
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
@@ -14,6 +21,7 @@ const Register: FC = () => {
       setPreviewUrl(URL.createObjectURL(file));
     }
   };
+  const handleButtonClick = () => console.log(selectedFile, previewUrl, username, email, password);
   return (
     <main className="register">
       <section className="register-section">
@@ -24,10 +32,38 @@ const Register: FC = () => {
             {previewUrl && <img src={previewUrl} alt="" className="register-form__profile" />}
             <input type="file" accept="image/*" src="profile-img" onChange={handleFileChange} />
           </div>
-          <InputText type="text" label="Username" placeholder="Your username..." />
-          <InputText type="email" label="Email" placeholder="Your email..." />
-          <InputText type="password" label="Password" placeholder="Your password..." />
-          <Button view="accent" onClick={() => {}}>
+          <InputText
+            type="text"
+            name="username"
+            label="Username"
+            placeholder="Your username..."
+            value={username}
+            onChange={handleUsernameChange}
+            onClear={() => setUsername("")}
+            minLength={6}
+            maxLength={20}
+          />
+          <InputText
+            type="email"
+            name="email"
+            label="Email"
+            placeholder="Your email..."
+            value={email}
+            onChange={handleEmailChange}
+            onClear={() => setEmail("")}
+          />
+          <InputText
+            type="password"
+            name="email"
+            label="Password"
+            placeholder="Your password..."
+            value={password}
+            onChange={handlePasswordChange}
+            onClear={() => setPassword("")}
+            minLength={6}
+            maxLength={30}
+          />
+          <Button view="accent" onClick={handleButtonClick}>
             Register
           </Button>
         </form>
