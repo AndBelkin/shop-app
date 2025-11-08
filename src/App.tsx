da798938e9a368a13baf2dfaaccc84e06ff43baa
AndBelkin <dev.andreubelkin@gmail.com>
2025-11-08 11:44:10 +0300

    add Routes, change header buttons click => navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import type { FC } from "react";
 import Header from "./layouts/header/Header";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 const App: FC = () => {
   return (
     <div className="app">
       <Header />
+      <Routes>
+        <Route index element={<div />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 };
--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -3,17 +3,19 @@ import Logotype from "../../components/logotype/Logotype";
 import SearchInput from "../../components/search/SearchInput";
 import Button from "../../components/button/Button";
 import "./Header.css";
+import { useNavigate } from "react-router-dom";
 
 const Header: FC = () => {
+  const navigate = useNavigate();
   return (
     <header className="header">
       <Logotype />
       <SearchInput />
       <div className="header-wrapper">
-        <Button view="white" onClick={() => {}}>
+        <Button view="white" onClick={() => navigate("/login")}>
           Login
         </Button>
-        <Button view="black" onClick={() => {}}>
+        <Button view="black" onClick={() => navigate("/register")}>
           Sign Up
         </Button>
       </div>
