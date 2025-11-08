8d8ae3545a4eb16a5672b16042d456e75b355068
AndBelkin <dev.andreubelkin@gmail.com>
2025-11-08 22:39:06 +0300

    created Menu

--- src/pages/menu/Menu.tsx
+++ src/pages/menu/Menu.tsx
@@ -1,10 +1,13 @@
 import type { FC } from "react";
 import PromoList from "../../layouts/promo/PromoList";
+import MenuList from "../../layouts/menu-list/MenuList";
+import "./Menu.css";
 
 const Menu: FC = () => {
   return (
     <main className="menu">
       <PromoList />
+      <MenuList />
     </main>
   );
 };
